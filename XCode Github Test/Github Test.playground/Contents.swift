499c3ef26b40c761664b7bb4d961f38fb85d3c28
Alexandra <alexandrarumell@gmail.com>
2016-06-24 15:36:16 -0400

    Looping Arrays in Arrays

--- XCode Github Test/Github Test.playground/Contents.swift
+++ XCode Github Test/Github Test.playground/Contents.swift
@@ -6,12 +6,54 @@ var str = "Hello, playground"
 
 var hello = "Hello! "
 
-var people = [["Ariel", "Ally", "Sosi"], ["lemonade", "lime", "gatorade"], ["cats", "dogs", "birds"]]
+var people = [(name:["Ariel", "Ally", "Sosi"]), (drink:["lemonade", "lime", "gatorade"]), (pet: ["cats", "dogs", "birds"])]
 
-print("Our names are " + (people[0].joinWithSeparator(" and ")) + "and we enjoy drinking " + (people[1].joinWithSeparator( " and ")) + "and we love our pet " + (people[2].joinWithSeparator( " and ")))
+print("Our names are " + (people[0].joinWithSeparator(" and ")) + " and we enjoy drinking " + (people[1].joinWithSeparator( " and ")) + " and we love our pet " + (people[2].joinWithSeparator( " and ")))
 
 
 var firstpart = (people[0].joinWithSeparator(" and "))
+
+var ariel = ("My name is " + (people[0][0]) + " and I enjoy drinking " + (people[1][0]))
+
+var ally = ("My name is " + (people[0][1]) + " and I enjoy drinking " + (people[1][1]))
+
+var sosi = ("My name is " + (people[0][2]) + " and I enjoy drinking " + (people[1][2]))
+//
+//for person in people{
+//    print("My name is " (people.allValues([0])))
+//                        }
+for person in people 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
