de10c2aabba365d871cb3d9048e621b40343986d
Alexandra <alexandrarumell@gmail.com>
2016-06-24 14:19:03 -0400

    Got hang of arrays in array

--- XCode Github Test/Github Test.playground/Contents.swift
+++ XCode Github Test/Github Test.playground/Contents.swift
@@ -3,3 +3,12 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var hello = "Hello! "
+
+var people = [["Ariel", "Ally", "Sosi"], ["lemonade", "lime", "gatorade"], ["cat", "dog", "bird"]]
+
+
+
+
+
