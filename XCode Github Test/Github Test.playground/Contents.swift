694cfcded8bc5641fa85714c7258647a9ed9a373
Alexandra <alexandrarumell@gmail.com>
2016-06-24 14:58:14 -0400

    Cracked Code

--- XCode Github Test/Github Test.playground/Contents.swift
+++ XCode Github Test/Github Test.playground/Contents.swift
@@ -6,7 +6,19 @@ var str = "Hello, playground"
 
 var hello = "Hello! "
 
-var people = [["Ariel", "Ally", "Sosi"], ["lemonade", "lime", "gatorade"], ["cat", "dog", "bird"]]
+var people = [["Ariel", "Ally", "Sosi"], ["lemonade", "lime", "gatorade"], ["cats", "dogs", "birds"]]
+
+print("Our names are " + (people[0].joinWithSeparator(" and ")) + "and we enjoy drinking " + (people[1].joinWithSeparator( " and ")) + "and we love our pet " + (people[2].joinWithSeparator( " and ")))
+
+
+var firstpart = (people[0].joinWithSeparator(" and "))
+
+
+
+
+
+
+
 
 
 
